a763be39fc0383e302b8f2c3693650c2824ab4a6
Ajay patel <patel.ajay745@gmail.com>
2024-10-08 18:35:16 -0400

    added appwrite auth

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import conf from "../config";
+
+import { Client, Account, ID } from "appwrite";
+
+export class Authservice {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique,
+        email,
+        password,
+        name
+      );
+
+      if (!userAccount) {
+        return userAccount;
+      }
+
+      return this.login({ email, password });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new Authservice();
+
+export default authService;
