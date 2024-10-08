a7e86718de8fa10f46c2246e06a28544a1a65306
Ajay patel <patel.ajay745@gmail.com>
2024-10-08 18:10:19 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import config from "./config";
+
+function App() {
+  console.log(config.appwriteUrl);
+
+  return <></>;
+}
+
+export default App;
